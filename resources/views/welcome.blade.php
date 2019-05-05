11daaf14c1265d6c9d047cc27d6ce9d2458a695c
Unknown <peter.molnar.h@gmail.com>
2019-05-05 16:07:19 +0200

    Setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="/css/bootstrap.css">
+        <link rel="stylesheet" href="/css/cv.css">
+        <title>CV</title>
+
+    </head>
+    <body class="">
+        <div class="container">
+            <header class="jumbotron col-sm-12 col-xl-10 shadow-sm p-3 mb-5 bg-white rounded text-center">
+                <div class="col-sm-8">
+                    <h1>Molnár Péter</h1>
+                    <img src="images/computer.png" alt="" width="64" height="64">
+                    <h3>Computer science engineer</h3>
+                </div>
+                <div class="col-sm-4">
+                    <img class="profileimg img-thumbnail" src="images/hokos.png" alt="" width="128" height="128">
+                </div>
+            </header>
+            <main class="col-sm-12 col-xl-10">
+                <div class="text col-sm-8 col-xl-8">
+                    <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:
+
+“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”
+The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.
+
+The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.</p>
+                </div>
+                <aside class="sider col-sm-4 col-xl-4">
+                    <ul>
+                        <li>asdf</li>
+                        <li>fdsghs</li>
+                        <li>sdfg</li>
+                        <li>sdfg</li>
+                    </ul>
+                </aside2>
+            </main>
+            <footer>
+
+            </footer>
+        </div>
+    </body>
+</html>
